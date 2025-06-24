162d7b8c7abdb8e6a81c3c5d856e89fa09e42b8f
Michael Wan <16.michael.09@gmail.com>
2025-06-24 23:30:00 +0200

    Fix merge conflict and add health check endpoint

--- index.js
+++ index.js
@@ -48,7 +48,6 @@ client.on('ready', () => {
 });
 
 // Simple HTTP server to keep Render happy
-const http = require('http');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
